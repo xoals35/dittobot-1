4e82c5b12a66a29f878b530ae5dc21b43c0b0b7d
ditto7890 <kwak071219@gmail.com>
2020-04-22 22:52:40 +0900

    delete api

--- public/js/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('api_link').addEventListener('click', (e) => {
-    let apiOpen = window.open('/api/v1/bot', '_blank');
-    apiOpen.focus();
-});
-
-document.getElementById('invite_link').addEventListener('click', (e) => {
-    let inviteOpen = window.open('/invite', '_blank');
-    inviteOpen.focus();
-});
