d2cb7b24d67fbed50038d08670027eb4c4494980
ditto7890 <kwak071219@gmail.com>
2020-04-23 12:57:44 +0900

    maybe last commit 22...

--- commands/command/email.js
+++ commands/command/email.js
@@ -1,8 +1,9 @@
 const mail = require("nodemailer")
+const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: "email",
-    aliases: ['mail', '드먀ㅣ', 'gmail', '이메일'],
+    aliases: ['mail', '드먀ㅣ', 'gmail', '이메일', '메일', 'dlapdlf'],
     category: 'command',
     run: async (client, message, args) => {
         if (!args[0]) return
@@ -24,12 +25,12 @@ module.exports = {
             from: process.env.EMAIL_USER,
             to: args[0],
             subject: titleAndDescription[0],
-            text: `${titleAndDescription[1]}\n\n--------------------------\n디스코드 유저 ${message.author.tag} 님이 디스코드 봇 ${client.user.username} 으로 보낸 메일입니다.`
+            text: `${titleAndDescription[1]}\n\n--------------------------\n디스코드 유저 ${message.author.tag} 님이 디스코드 봇 ${client.user.tag} 으로 보낸 메일입니다.`
         }, (err) => {
             if (err) {
                 message.channel.send('Error...')
                 return console.log(err)
-            } else message.channel.send(`\`${args[0]}\`(으)로 제목: \`${titleAndDescription[0]}\`, 내용: \`${titleAndDescription[1]}\` 을 보냈습니다!`)
+            } else message.channel.send(new MessageEmbed().setTitle('메일 전송').setColor(0xFFFF00).setDescription(`**받는 사람**\n\`\`\`${args[0]}\`\`\`\n**제목**\n\`\`\`fix\n${titleAndDescription[0]}\n\`\`\`\n**내용**\`\`\`yml\n${titleAndDescription[1]}\n\`\`\``))
         })
     }
 }
